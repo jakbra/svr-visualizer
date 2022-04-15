4a93e9fe980d583ce81a307af3c1b21ccf15bf6e
Jakob <bracun.jakob@gmail.com>
2022-04-15 08:08:14 +0200

    Shanged size

--- svr.py
+++ svr.py
@@ -7,10 +7,10 @@ class MainApplication(tk.Frame):
     def __init__(self, master, *args, **kwargs):
 
         tk.Frame.__init__(self, master, *args, **kwargs)
-        self.width = 1000
+        self.width = 500
         self.master = master
         self.frame = tk.Frame(self.master)
-        self.canvas = tk.Canvas(self.frame, height=1000, width=self.width, bg = "white")
+        self.canvas = tk.Canvas(self.frame, height=500, width=self.width, bg = "white")
 
         self.draw_flag = False
 
@@ -32,14 +32,14 @@ class MainApplication(tk.Frame):
         #Sliders, button, frame, canvas
         self.glabel = tk.Label(self.master, text = "gamma")
         self.glabel.grid(row=0,column=1, sticky="n")
-        self.gslider = tk.Scale(self.master, from_=0.001, to=0.003,resolution = 0.0001, orient="vertical", length = 800, sliderlength = 60)
+        self.gslider = tk.Scale(self.master, from_=0.001, to=0.003,resolution = 0.0001, orient="vertical", length = 400, sliderlength = 30)
         self.gslider.bind("<ButtonRelease-1>", self.set)
         self.gslider.set(0.002)
         self.gslider.grid(row = 0, column=1)
 
         self.clabel = tk.Label(self.master, text = "C")
         self.clabel.grid(row=0,column=2, sticky="n")
-        self.cslider = tk.Scale(self.master, from_=1, to=300, orient="vertical", length = 800, sliderlength = 60)
+        self.cslider = tk.Scale(self.master, from_=1, to=300, orient="vertical", length = 400, sliderlength = 30)
         self.cslider.bind("<ButtonRelease-1>", self.set)
         self.cslider.set(150)
         self.cslider.grid(row = 0, column=2)
@@ -47,7 +47,7 @@ class MainApplication(tk.Frame):
 
         self.plabel = tk.Label(self.master, text = "epsilon")
         self.plabel.grid(row=0,column=3, sticky="n")
-        self.pslider = tk.Scale(self.master, from_=0.0001, to=10,resolution = 1, orient="vertical", length = 800, sliderlength = 60)
+        self.pslider = tk.Scale(self.master, from_=0.0001, to=10,resolution = 1, orient="vertical", length = 400, sliderlength = 30)
         self.pslider.set(5)
         self.pslider.bind("<ButtonRelease-1>", self.set)
         self.pslider.grid(row = 0, column=3)
@@ -102,7 +102,7 @@ class MainApplication(tk.Frame):
         for i in zip(self.x_list,self.y_list):
             x_dot = i[0][0]
             y_dot = i[1]
-            self.canvas.create_oval((x_dot-1),(y_dot-1), (x_dot+1),(y_dot+1), fill="red", width=10)
+            self.canvas.create_oval((x_dot-1),(y_dot-1), (x_dot+1),(y_dot+1), fill="red", width=7)
         
 
     def show_line(self):
@@ -114,7 +114,7 @@ class MainApplication(tk.Frame):
         for i in zip(self.x_plot,self.y_plot):
             x = i[0][0]
             y = i[1][0]
-            self.canvas.create_oval((x-1),(y-1), (x+1),(y+1), fill="red", tags="line")
+            self.canvas.create_oval((x-1),(y-1), (x+1),(y+1), fill="red", tags="line", width=1)
                     
             
 
